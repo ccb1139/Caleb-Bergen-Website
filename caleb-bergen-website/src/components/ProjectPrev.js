b243e4552e0fad9df948db1986d8ab7a40b29b3e
ccb1139 <calebbergen000@gmail.com>
2023-02-22 22:12:46 -0500

    styf

--- caleb-bergen-website/src/components/ProjectPrev.js
+++ caleb-bergen-website/src/components/ProjectPrev.js
@@ -1,26 +1,32 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Image } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Overlay from 'react-bootstrap/Overlay';
+import React, { useState, useEffect, useRef } from "react";
+import { Image } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Overlay from "react-bootstrap/Overlay";
 import { Link } from "react-router-dom";
-import Tooltip from 'react-bootstrap/Tooltip';
+import Tooltip from "react-bootstrap/Tooltip";
+import Collapse from "react-bootstrap/Collapse";
+import { AiOutlineDown } from "react-icons/ai";
 
-function ProjectPrev({title, img, description, link}) {
+function ProjectPrev({ title, img, description, link }) {
   const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
   const target = useRef(null);
 
-
   return (
-    <Link to="/projects" className='reset-a col-4' >
-        <div className='prog-prev ' >
+    <div className="col-4 prog-prev cursor-change" style={{cursor:"pointer"}}>
+        <Link to="/projects" className="reset-a col-4 cursor-change">
             <h5>{title}</h5>
-            <Image fluid src={img} rounded className="hover-shadow m-auto m-1" style={{height:"150px", margin:"auto"}}/>
-            <p>{description}</p> 
-        </div>
-    </Link>
+            <Image
+            fluid
+            src={img}
+            rounded
+            className="hover-shadow m-auto m-1"
+            style={{ height: "150px", margin: "auto" }}
+            />
+        </Link>
     
-    
-  )
+    </div>
+  );
 }
 
-export default ProjectPrev
+export default ProjectPrev;
--- caleb-bergen-website/src/components/WhatImBestAt.js
+++ caleb-bergen-website/src/components/WhatImBestAt.js
@@ -3,9 +3,9 @@ import { Image } from 'react-bootstrap';
 
 function WhatImBestAt({title, img}) {
   return (
-    <div className='col-4 p-3'>
+    <div className='col-2 p-3'>
         <h5>{title}</h5>
-        <Image fluid src={img} rounded className="hover-shadow m-auto" style={{maxHeight:"150px", margin:"auto"}}/>
+        <Image fluid src={img} rounded className="hover-shadow m-auto" style={{maxHeight:"60px", margin:"auto"}}/>
 
     </div>
   )
